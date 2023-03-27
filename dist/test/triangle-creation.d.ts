143963404fb4c567988d16bd24701b52d60f8fe3
LuizHAOliveira <78063465+LuizHAOliveira@users.noreply.github.com>
2023-03-26 22:17:18 -0300

    Delete dist directory

--- dist/test/triangle-creation.d.ts
+++
@@ -1,3 +0,0 @@
-import { TriangleFactory } from '../src/basic-classes';
-export declare function triangleFactoryCreation(data: string): TriangleFactory;
-export declare function readFileAndTest(testFunction: Function): void;
